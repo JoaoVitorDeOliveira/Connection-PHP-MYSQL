205cad9c3026e2abeb4a9ccb512d9e184d9ed3ee
joao <jvitordeoliveiras@gmail.com>
2018-09-20 00:14:53 -0300

    em prod

--- php/disciplinasTelaPrincipal.php
+++ php/disciplinasTelaPrincipal.php
@@ -7,7 +7,19 @@
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="../css/stylesDisciplinasTelaPrincipal.css" />
-    <script src="main.js"></script>
+    <script>
+        function myFunction() {
+            var x;
+
+            var site = prompt("Digite o nome da matéria:", "");
+
+            if (site != null) {
+                x = "`" + site + "`";
+                document.getElementById("demo").innerHTML = x;
+            }
+    }
+
+    </script>
 </head>
 <body>
 
@@ -15,10 +27,13 @@
             João Vitor de Oliveira </br></br>
             317113712
     </div>
-    <div class="lateral"></div>
-   <section>
-   <h1>Disciplinas</h1>
+    <h1>Disciplinas</h1>
+    <div class="box">
     <ul>
+        <div class="crud">
+            <button onclick="" class="del">x</button>
+            <button onclick="myFunction()" class="add">+</button>
+        </div>
 		<?php 
 					//Preenchendo a tabela com os dados
 					$query = mysqli_query($conectar, "SELECT `Disciplinas` FROM TB_DISCIPLINAS");
@@ -29,9 +44,11 @@
 							<input type='submit' name='mat' value='{$i['Disciplinas']}'>
 						</form></h2></div></li>";	
                 }
+                echo "<spam id='demo'></spam>";
 		?>
     </ul>
-	</section>
+    </div>
+
 </body>
 </html>
 
