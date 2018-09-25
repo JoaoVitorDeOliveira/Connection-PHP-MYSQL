0604338d18ec7c3124ade1f3aa568ad7483ffb3b
joao <jvitordeoliveiras@gmail.com>
2018-09-24 23:27:35 -0300

    em prod

--- php/criarTabelasDeDisciplinasGenericas.php
+++ php/criarTabelasDeDisciplinasGenericas.php
@@ -23,7 +23,7 @@
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="../css/stylesCriarTabelasGenericas.css" /> 
-        <script >
+        <script>
             function escondeEsconde() {
             var x = document.getElementById('respostas');
             if (x.style.display === 'none') {
