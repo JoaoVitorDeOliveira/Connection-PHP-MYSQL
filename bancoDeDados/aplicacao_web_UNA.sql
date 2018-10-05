f1fe085de3e91052c98bbb0162baeffd7512fa72
João Vitor de Oliveira [e] <joliveira@fitec.org.br>
2018-10-05 16:22:10 -0300

    bd novo

--- bancoDeDados/aplicacao_web_UNA.sql
+++ bancoDeDados/aplicacao_web_UNA.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 01-Out-2018 às 20:46
+-- Generation Time: 05-Out-2018 às 21:21
 -- Versão do servidor: 10.1.35-MariaDB
 -- versão do PHP: 7.2.9
 
@@ -40,10 +40,11 @@ CREATE TABLE `tb_cadastro_alunos` (
 
 INSERT INTO `tb_cadastro_alunos` (`RA`, `NOME`, `SENHA`) VALUES
 (317113712, 'Joao Vitor de Oliveira', 'oliveira'),
-(317113713, 'Maria Clara', '123'),
 (317113714, 'Marcus Augusto', '1234'),
-(3476584832, 'PEDRO', '123'),
-(3476584834, 'MARIA', '987');
+(3476584834, 'MARIA', '987'),
+(317113713, 'Maria Clara', '123'),
+(123, 'NinguÃ©m te', '123'),
+(3476584832, 'PEDRO', '123');
 
 -- --------------------------------------------------------
 
@@ -93,13 +94,13 @@ CREATE TABLE `tb_perguntas` (
 INSERT INTO `tb_perguntas` (`PerguntasID`, `DisciplinasID`, `Perguntas`, `NomesAlunos`) VALUES
 (1, 5, 'Qual a derivada de 2x + 3', ''),
 (3, 5, 'Regra do produto eh pra derivada ?', ''),
-(4, 11, 'O que é uma classe ?', ''),
-(5, 11, 'O que é um objeto ?', ''),
 (6, 4, 'O que são eletrons ?', ''),
 (11, 5, 'Qual a Derivada de 3x + 2 ?', 'Joao Vitor de Oliveira'),
 (12, 1, 'O que Ã© integrar ?', 'PEDRO'),
 (13, 8, 'Como faz transformada em Z ?', 'Maria Clara'),
-(14, 5, 'AlguÃ©m tem o link de um livro de CÃ¡lculo ?', 'Maria Clara');
+(14, 5, 'AlguÃ©m tem o link de um livro de CÃ¡lculo ?', 'Maria Clara'),
+(15, 8, 'AlguÃ©m tem alguma prova antiga de Sinais ?', 'Marcus Augusto'),
+(16, 11, 'O que Ã© uma instancia de uma classe ?', 'Maria Clara');
 
 -- --------------------------------------------------------
 
@@ -119,11 +120,6 @@ CREATE TABLE `tb_respostas` (
 --
 
 INSERT INTO `tb_respostas` (`RespostasID`, `PerguntasID`, `Respostas`, `NomesAlunos`) VALUES
-(1, 1, 'O resultado da 2', ''),
-(2, 1, 'Tem certeza ?', ''),
-(3, 1, 'Siim!!', ''),
-(4, 3, 'Siim!!', ''),
-(7, 1, 'Ta certo!', ''),
 (8, 1, 'ta nÃ£o', 'Joao Vitor de Oliveira'),
 (9, 1, 'vcs nÃ£o sabem de nada', 'PEDRO'),
 (10, 12, 'Ã© tipo derivar sÃ³ que contrario', 'Joao Vitor de Oliveira'),
@@ -135,10 +131,13 @@ INSERT INTO `tb_respostas` (`RespostasID`, `PerguntasID`, `Respostas`, `NomesAlu
 (16, 13, 'https://www.todamateria.com.br/teorema-de-laplace/', 'Marcus Augusto'),
 (17, 13, 'Valeu!!', 'Joao Vitor de Oliveira'),
 (18, 13, 'mas e a transformada em Z gente ?!?!', 'Maria Clara'),
-(19, 5, 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn-istoedinheiro-ssl.akamaized.net%2Fwp-content%2Fuploads%2Fsites%2F17%2F2018%2F02%2Fpalio.jpg&imgrefurl=https%3A%2F%2Fwww.istoedinheiro.com.br%2Fcarro-barato-sai-do-foco-e-fiat-aposenta-o-palio%2F&docid=P8jmWe01pS2QiM&tbnid=BH-O6LydDB47FM%3A&vet=10ahUKEwjerYu14OXdAhUKkJAKHa7MDF0QMwgmKAEwAQ..i&w=1024&h=576&client=firefox-b-ab&bih=574&biw=1268&q=carro&ved=0ahUKEwjerYu14OXdAhUKkJAKHa7MDF0QMwgmKAEwAQ&iact=mrc&uact=8', 'Maria Clara'),
 (20, 14, 'http://una.bv3.digitalpages.com.br/users/publications/9788543005423/pages/-12', 'Joao Vitor de Oliveira'),
 (21, 14, 'Na biblioteca da Una tem esse', 'Joao Vitor de Oliveira'),
-(22, 11, 'sim, valeu!!', 'Joao Vitor de Oliveira');
+(22, 11, 'sim, valeu!!', 'Joao Vitor de Oliveira'),
+(28, 15, 'NÃ£o tenho...', 'Maria Clara'),
+(32, 16, 'Uma instÃ¢ncia de uma classe Ã© um novo objeto criado dessa classe, com o operador new. \r\n\r\nInstanciar uma classe Ã© criar um novo objeto do mesmo tipo dessa classe.', 'Joao Vitor de Oliveira'),
+(39, 16, 'Em programaÃ§Ã£o orientada a objetos, chama-se instÃ¢ncia de uma classe, um objeto cujo comportamento e estado sÃ£o definidos pela classe.\r\n\"InstÃ¢ncia\" Ã©, neste caso, um anglicismo, significando \"caso\" ou \"exemplo\" (em inglÃªs instance).\r\n\r\nAs instÃ¢ncias de uma classe compartilham o mesmo conjunto de atributos, embora sejam diferentes quanto ao conteÃºdo desses atributos.\r\nPor exemplo, a classe \"Empregado\" descreve os atributos comuns a todas as instÃ¢ncias da classe \"Empregado\". Os objetos dessa classe podem ser semelhantes, mas variam em atributos tais como \"nome\" e \"salÃ¡rio\". A descriÃ§Ã£o da classe contÃ©m os itens correspondentes a esses atributos e define as operaÃ§Ãµes ou aÃ§Ãµes relevantes para a classe, tais como \"aumento de salÃ¡rio\" ou \"mudanÃ§a do nÃºmero de telefone\". Pode-se entÃ£o falar sobre uma instÃ¢ncia com o nome = \"Joana Coelho\" e outra com o nome = \"JoÃ£o Coelho\".\r\n\r\nInstÃ¢ncia Ã© a concretizaÃ§Ã£o de uma classe. Em termos intuitivos, uma classe Ã© como um \"molde\" que gera instÃ¢ncias de um certo tipo; um objeto Ã© algo que existe fisicamente e que foi \"moldado\" na classe.\r\n\r\nAssim, em programaÃ§Ã£o orientada a objetos, a palavra \"instanciar\" significa criar. Quando falamos em \"instanciar um objeto\", criamos fisicamente uma representaÃ§Ã£o concreta da classe. Por exemplo: \"animal\" Ã© uma classe ou um molde; \"cachorro\" Ã© uma instÃ¢ncia de \"animal\" e apesar de carregar todas as caracterÃ­sticas do molde de \"animal\", Ã© completamente independente de outras instÃ¢ncias de \"animal\". Exemplo em C#:', 'Joao Vitor de Oliveira'),
+(40, 14, 'O cÃ¡lculo diferencial e integral, ou simplesmente cÃ¡lculo, Ã© um ramo importante da matemÃ¡tica, desenvolvido a partir da Ãlgebra e da Geometria, que se dedica ao estudo de taxas de variaÃ§Ã£o de grandezas (como a inclinaÃ§Ã£o de uma reta) e a acumulaÃ§Ã£o de quantidades (como a Ã¡rea debaixo de uma curva ou o volume de um sÃ³lido). Onde hÃ¡ movimento ou crescimento em que forÃ§as variÃ¡veis agem produzindo aceleraÃ§Ã£o, o cÃ¡lculo Ã© a matemÃ¡tica a ser empregada.\r\n\r\n\r\nO cÃ¡lculo permite calcular a Ã¡rea da regiÃ£o assinalada.\r\nO cÃ¡lculo foi criado como uma ferramenta auxiliar em vÃ¡rias Ã¡reas das ciÃªncias exatas. Desenvolvido simultaneamente por Gottfried Wilhelm Leibniz (1646-1716) e por Isaac Newton (1643-1727), em trabalhos independentes. O CÃ¡lculo Diferencial e o CÃ¡lculo Integral auxiliam em vÃ¡rios conceitos e definiÃ§Ãµes na matemÃ¡tica, quÃ­mica, fÃ­sica clÃ¡ssica, fÃ­sica moderna e economia. O estudante de cÃ¡lculo deve ter um conhecimento em certas Ã¡reas da matemÃ¡tica, como funÃ§Ãµes (modular, exponencial, logarÃ­tmica, par, Ã­mpar, afim e segundo grau, por exemplo) , trigonometria, polinÃ´mios, geometria plana, espacial e analÃ­tica, pois sÃ£o a base do cÃ¡lculo. O cÃ¡lculo tem inicialmente trÃªs \"operaÃ§Ãµes-base\", ou seja, possui Ã¡reas iniciais como o cÃ¡lculo de limites, o cÃ¡lculo de derivadas de funÃ§Ãµes e a integral de diferenciais.\r\n\r\nA integral indefinida tambÃ©m pode ser chamada de antiderivada, uma vez que Ã© um processo que inverte a derivada de funÃ§Ãµes. JÃ¡ a integral definida, inicialmente definida como Soma de Riemann, estabelece limites de integraÃ§Ã£o, ou seja, Ã© um processo estabelecido entre dois intervalos bem definidos, daÃ­ o nome integral definida.\r\n\r\nCom o advento do Teorema Fundamental do CÃ¡lculo, estabeleceu-se uma conexÃ£o entre os dois ramos do cÃ¡lculo: o CÃ¡lculo Diferencial e o CÃ¡lculo Integral. O cÃ¡lculo diferencial surgiu do problema da tangente, enquanto o cÃ¡lculo integral surgiu de um problema aparentemente nÃ£o relacionado, o problema da Ã¡rea. O professor de Isaac Newton em Cambridge, Isaac Barrow, descobriu que esses dois problemas estÃ£o de fato estritamente relacionados, ao perceber que a derivaÃ§Ã£o e a integraÃ§Ã£o sÃ£o processos inversos. Foram Leibniz e Newton que exploraram essa relaÃ§Ã£o e a utilizaram para transformar o cÃ¡lculo em um mÃ©todo matemÃ¡tico sistemÃ¡tico. Particularmente ambos viram que o Teorema Fundamental os capacitou a calcular Ã¡reas e integrais muito mais facilmente, sem que fosse necessÃ¡rio calculÃ¡-las como limites de soma (mÃ©todo descrito pelo matemÃ¡tico Riemann, pupilo de Gauss)', 'Joao Vitor de Oliveira');
 
 --
 -- Indexes for dumped tables
@@ -148,7 +147,8 @@ INSERT INTO `tb_respostas` (`RespostasID`, `PerguntasID`, `Respostas`, `NomesAlu
 -- Indexes for table `tb_cadastro_alunos`
 --
 ALTER TABLE `tb_cadastro_alunos`
-  ADD PRIMARY KEY (`RA`);
+  ADD PRIMARY KEY (`RA`),
+  ADD UNIQUE KEY `NOME` (`NOME`,`SENHA`);
 
 --
 -- Indexes for table `tb_disciplinas`
@@ -178,19 +178,19 @@ ALTER TABLE `tb_respostas`
 -- AUTO_INCREMENT for table `tb_disciplinas`
 --
 ALTER TABLE `tb_disciplinas`
-  MODIFY `DisciplinasID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `DisciplinasID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tb_perguntas`
 --
 ALTER TABLE `tb_perguntas`
-  MODIFY `PerguntasID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `PerguntasID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `tb_respostas`
 --
 ALTER TABLE `tb_respostas`
-  MODIFY `RespostasID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `RespostasID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- Constraints for dumped tables
